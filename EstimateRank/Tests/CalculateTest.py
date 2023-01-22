352ed6a4974bc4cdd18932e9a74f929dc59e7b6c
melikasojodi <melikasojodi@gmail.com>
2023-01-22 23:49:25 +0330

    .

--- EstimateRank/Tests/CalculateTest.py
+++ EstimateRank/Tests/CalculateTest.py
@@ -5,6 +5,7 @@ from time import sleep
 import unittest
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class CalculateTest(unittest.TestCase):
@@ -12,7 +13,7 @@ class CalculateTest(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.chrome_options = Options()
         cls.chrome_options.add_argument("--headless")
-        cls.service = Service(executable_path="C:\chromedriver.exe")
+        cls.service = Service(executable_path=ChromeDriverManager().install())
         cls.driver = webdriver.Chrome(service=cls.service, chrome_options=cls.chrome_options)
         cls.driver.implicitly_wait(3)
         cls.driver.maximize_window()
